3f14f47a750e8cd06c4ff7e09c1fdc4fb9091f19
TodorKV <todorv1@gmail.com>
2022-11-12 11:30:58 +0200

    build

--- target/generated-sources/annotations/com/ft/mappers/PhotoMapperImpl.java
+++ target/generated-sources/annotations/com/ft/mappers/PhotoMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-12T10:56:40+0200",
+    date = "2022-11-12T11:30:09+0200",
     comments = "version: 1.5.0.Beta1, compiler: javac, environment: Java 11.0.17 (Ubuntu)"
 )
 @Component
--- target/generated-sources/annotations/com/ft/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/ft/mappers/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-12T10:56:41+0200",
+    date = "2022-11-12T11:30:08+0200",
     comments = "version: 1.5.0.Beta1, compiler: javac, environment: Java 11.0.17 (Ubuntu)"
 )
 @Component
